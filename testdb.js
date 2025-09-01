f3f154e81ac500695ea9929ba642aaf65274d859
rahulktiwari3009-creator <rahulktiwari3009@gmail.com>
2025-09-02 02:39:19 +0530

    testdb.js

---
+++ testdb.js
@@ -0,0 +1,12 @@
+const pool = require('./db');
+
+async function testConnection() {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log("✅ Database connected:", res.rows[0]);
+  } catch (err) {
+    console.error("❌ Database connection error:", err);
+  }
+}
+
+testConnection();
